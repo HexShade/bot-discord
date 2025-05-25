e2c361cef98009fd8ef6f8c53470ab1c9bd0e201
HexShade <kangarotcr@gmail.com>
2025-05-25 12:47:20 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot('/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Manito esta on')
+
+@bot.command()
+async def Batidao(ctx:commands.Context):
+    nome = ctx.author.global_name
+    icone = ctx.author.avatar
+    await ctx.reply(f'{icone} Pode deixar {nome}')
+
+@bot.command()
+async def Falar(ctx:commands.Context, texto):
+    await ctx.reply(texto)
+
+bot.run('MTM3NjE4NjU2MjAxODI4MzcwMw.Geu8DW.-1qT8XiG4s99EE7ysdufQeRbJMbS3nGrC3wRBc')
